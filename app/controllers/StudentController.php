4a5a60b71ffd3b7b60122559bbb94e99dcb52a02
Jopannnn <jov132009@gmail.com>
2026-04-14 08:39:03 +0700

    wip: display students

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 require_once '../app/core/controller.php';
+require_once '../app/models/student.php';
 
 use App\Core\Controller;
+use App\models\Student;
 
 class StudentController extends Controller
 {
     public function index()
     {
+      $StudentModel = new Student();
+      $students = $StudentModel->getStudents();
+      print_r($students);
        $this->view('students.index');
 
     }
---
+++ app/models/student.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\models;
+require_once '../app/core/Database.php';
+use App\core\Database;
+
+class Student extends Database
+{
+    protected $table = 'students';
+
+    public function getStudents()
+    {
+        $students = [];
+        $query = "SELECT * FROM {$this->table}";
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while ($student=$result->fetch_assoc()) 
+        {
+            $students[] = $student;
+        }
+        return $students;
+
+    }
+
+}
+
+
+?>
