a6fe9c4ddd16f3965b7609ed85a09743ac145f9d
Jopannnn <jov132009@gmail.com>
2026-04-17 11:08:33 +0700

    feat: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -10,39 +10,41 @@ use App\models\Student;
 
 class StudentController extends Controller
 {
-    public function index()
-    {
-      $StudentModel = new Student();
-      $students = $StudentModel->getStudents();
-      print_r($students);
-       $this->view('students.index');
+      public function index()
+      {
+            $StudentModel = new Student();
+            $students = $StudentModel->getStudents();
 
-    }
+            $this->view('students.index', [
+                  'students' => $students
+            ]);
+
+      }
 
 
 
 
 
-    public function create()
-    {
+      public function create()
+      {
 
-          $this->view('students.create');
+            $this->view('students.create');
 
-    }
+      }
 
-    public function show($id)
-    {
-       
-          $this->view('students.show');
+      public function show($id)
+      {
 
-    }
+            $this->view('students.show');
 
-     public function edit($id)
-     {
-          
-          $this->view('students.edit');
-     
-     }
+      }
+
+      public function edit($id)
+      {
+
+            $this->view('students.edit');
+
+      }
 }
-    
+
 ?>
